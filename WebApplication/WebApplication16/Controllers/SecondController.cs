2bd388ec281e662eef4bdb420122a257e074e904
moudroff <moudroff>
2019-02-15 19:59:01 +0300

    Init

---
+++ WebApplication/WebApplication16/Controllers/SecondController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication16.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: Second
+        public ActionResult Index()
+        {
+            ViewBag.MyKey = "Hello";
+
+
+            return View();
+        }
+    }
+}
