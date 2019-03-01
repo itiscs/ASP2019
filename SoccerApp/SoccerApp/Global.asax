08aff09f41599c9b6cb66b20ddff10c9d376b25d
moudroff <moudroff>
2019-03-01 17:30:56 +0300

    EF many entities

---
+++ SoccerApp/SoccerApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoccerApp.MvcApplication" Language="C#" %>
---
+++ SoccerApp/SoccerApp/Models/SoccerDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SoccerApp.Models
+{
+    public class SoccerDB : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
