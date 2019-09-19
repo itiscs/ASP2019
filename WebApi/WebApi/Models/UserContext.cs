2f678b03e3f1738575d02fe1992ce6db58e01db8
moudroff <moudroff>
2019-09-19 15:41:12 +0300

    Web Api

---
+++ WebApi/WebApi/Models/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApi.Models
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        { }
+    }
+}
