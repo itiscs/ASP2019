af8da966622cf3e2884170df3e4eff22be73afad
moudroff <moudroff>
2019-09-26 15:13:45 +0300

    Web Api Client

---
+++ WebApiClient/WebApiClient/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiClient.Models;
+using WebApiClient.Services;
+
+namespace WebApiClient.Controllers
+{
+    public class UsersController : Controller
+    {
+        UserService serv = new UserService();
+
+        public IActionResult Index()
+        {
+            var users = serv.GetUsers().Result;
+            return View(users);
+        }
+        public IActionResult Details(int id)
+        {
+            var user = serv.GetUsersByID(id).Result;
+            return View(user);
+        }
+
+        [HttpGet]
+        public ViewResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser([Bind("Name", "Age")] User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var res = await serv.AddUser(user);
+            return RedirectToAction("Index", "Users");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var user = serv.GetUsersByID(id).Result;
+            return View(user);
+        }
+
+        // POST: Phones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var res = await serv.DeleteUser(id);
+            return RedirectToAction("Index", "Users");
+        }
+
+    }
+}
---
+++ WebApiClient/WebApiClient/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApiClient.Models;
+
+namespace WebApiClient.Services
+{
+    public class UserService
+    {
+        HttpClient client = new HttpClient();
+        string uri = "https://localhost:44340/api/users";
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var resp = await client.GetAsync(uri);
+            var result = resp.Content.ReadAsStringAsync().Result;
+            var users = JsonConvert.DeserializeObject<List<User>>(result);
+            return users;
+        }
+
+        public async Task<User> GetUsersByID(int id)
+        {
+            var resp = await client.GetAsync(uri + $"/{id}");
+            var result = resp.Content.ReadAsStringAsync().Result;
+            var user = JsonConvert.DeserializeObject<User>(result);
+            return user;
+        }
+
+        public async Task<HttpStatusCode> AddUser(User user)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                uri, user);
+            return response.StatusCode;
+        }
+
+        public async Task<HttpStatusCode> DeleteUser(int id)
+        {
+
+            HttpResponseMessage response = await client.DeleteAsync(uri+$"/{id}");
+            return response.StatusCode;
+        }
+    }
+}
