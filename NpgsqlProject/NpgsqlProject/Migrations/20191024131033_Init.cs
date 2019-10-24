579ea52ed1f6457ce8d71e142dd12d7560dd6f98
moudroff <moudroff>
2019-10-24 17:54:20 +0300

    Npgsql

---
+++ NpgsqlProject/NpgsqlProject/Migrations/20191024131033_Init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace NpgsqlProject.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ NpgsqlProject/NpgsqlProject/Models/BooksContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NpgsqlProject.Models
+{
+    public class BooksContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public BooksContext(DbContextOptions<BooksContext> options)
+           : base(options)
+        {
+           // Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=BookStore;Username=postgres;Password=1");
+        }
+    }
+}
+
